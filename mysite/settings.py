7d32e8f7d04ba880661a10c4e37815bfab9c9d67
linzeyang <zeyanglin2013@gmail.com>
2013-04-20 22:19:38 +0100

    update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/zeyang/MetaSearch/Project/static',
+    '/home/repo/metasearch/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -110,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/zeyang/MetaSearch/Project/templates",
+    "/home/repo/metasearch/templates",
 )
 
 INSTALLED_APPS = (
